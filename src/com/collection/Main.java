c2ed1db50bd84bdd25534ab4cece9562ebca817e
Evgeniya Prudenkova <evgeniya@MacBook-Pro-16.local>
2021-03-06 11:25:38 -0500

    First commit

---
+++ src/com/collection/Main.java
@@ -0,0 +1,12 @@
+package com.collection;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        for(int i=0; i<5; 1++){
+            System.out.println("Hello");
+            Thread.sleep(2000);
+        }
+    }
+}
